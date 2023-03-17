1f88114f4dd79ca940433e464c45399439d7708c
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-03-17 15:18:09 +0900

    사각형

--- JumpIn_iOS/ViewController.swift
+++ JumpIn_iOS/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class ViewController: UIViewController {
+    
+    var abutton = UIButton().then {
+        $0.backgroundColor = .red
+    }
+    
+    var a: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(abutton)
+        view.backgroundColor = .systemBackground
+        
+        abutton.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+            $0.height.equalTo(50.0)
+            $0.width.equalTo(100)
+        }
+        
+        abutton.layer.cornerRadius = 10.0
     }
-
 
 }
 
